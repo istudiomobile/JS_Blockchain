363d16c72daf8ab55d05a605c92e8dd298155cd3
istudiomobile <es1410905775@unadmexico.mx>
2019-01-28 21:37:48 -0600

    Added Express.js to build the API and return the blockchain.

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+const express = require('express');
+const app = express();
+
 let Block = require('./block');
 let Blockchain = require('./blockChain');
 let Transaction = require('./transaction');
 
-let transaction = new Transaction('Mary', 'Jerry', 100);
-let genesisBlock = new Block();
-let blockchain = new Blockchain(genesisBlock);
+app.get('/', function(req, res) {
+    res.send('Hello API!');
+});
 
-let block = blockchain.getNextBlock([transaction]);
-blockchain.addBlock(block);
+app.get('/blockchain', function(req, res) {
 
-console.log(blockchain);
+    let transaction = new Transaction('Mary', 'Jerry', 100);
+    let genesisBlock = new Block();
+    let blockchain = new Blockchain(genesisBlock);
+
+    let block = blockchain.getNextBlock([transaction]);
+    blockchain.addBlock(block);
+
+    let second_transaction = new Transaction('Jerry', 'Jason', 10);
+    let block2 = blockchain.getNextBlock([second_transaction]);
+    blockchain.addBlock(block2);
+
+    res.json(blockchain);
+});
+
+app.listen(3000, function() {
+    console.log("Server listening on port 3000");
+});
+
+/* 
+
+console.log(blockchain); */
